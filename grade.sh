d203946076a067394d1126f3faf32ab98b5b98c5
Burak E Dikbas <cs15lwi23avh@ieng6-203.ucsd.edu>
2023-03-02 16:41:33 -0800

    Committed

--- grade.sh
+++ grade.sh
@@ -3,3 +3,17 @@ CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 rm -rf student-submission
 git clone $1 student-submission
 echo 'Finished cloning'
+
+cd student-submission
+
+if [[ -e ListExamples.java ]]
+then
+   echo 'File Found'
+else
+   echo 'FILE NOT FOUND'
+   exit
+fi
+
+cp student-submission/ListExamples.java ./
+javac -cp $CPATH *.java
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples
